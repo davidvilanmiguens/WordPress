1f9e1f44f8a8b75a9ca4e824de4108c8113ceb15
David Vilan Miguens <dvilanmiguens@danielcastelao.org>
2021-12-14 19:06:41 +0100

    error404 acabado

---
+++ html/wp-content/themes/newfancy/404.php
@@ -0,0 +1,25 @@
+<?php
+get_header();
+?>
+
+
+
+    <div id="contenedor" class="red">
+        <div id="cabecera">
+
+            <h1>Not found</h1>
+
+        </div>
+
+        <div id="cuerpo" class="red">
+
+            <img src="<?php echo get_stylesheet_directory_uri();?>/imagenes/Error.jpg">
+
+        </div>
+
+    </div>
+
+
+
+<?php
+get_footer();
